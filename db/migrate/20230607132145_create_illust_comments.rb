a42432c304bb292a33f88ec1dcc8ddc1a3442a2d
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-06-08 14:29:15 +0000

    [header]

--- db/migrate/20230607132145_create_illust_comments.rb
+++ db/migrate/20230607132145_create_illust_comments.rb
@@ -1,6 +1,9 @@
 class CreateIllustComments < ActiveRecord::Migration[6.1]
   def change
     create_table :illust_comments do |t|
+      t.text :comment
+      t.integer :user_id
+      t.integer :illust_id
 
       t.timestamps
     end
--- db/migrate/20230607132707_create_illusts.rb
+++ db/migrate/20230607132707_create_illusts.rb
@@ -1,6 +1,9 @@
 class CreateIllusts < ActiveRecord::Migration[6.1]
   def change
     create_table :illusts do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
 
       t.timestamps
     end
