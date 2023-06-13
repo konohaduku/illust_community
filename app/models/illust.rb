ba384f67dfc5d7b6152c1d4362fb4ba434103712
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-06-13 14:55:43 +0000

    [sign4]

--- app/models/illust.rb
+++ app/models/illust.rb
@@ -1,2 +1,9 @@
 class Illust < ApplicationRecord
+   has_one_attached :image
+     belongs_to :user
+     has_many :illust_comments, dependent: :destroy
+     has_many :favorites, dependent: :destroy
+
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :illusts, only: [:new,:index,:show,:edit,:create,:destroy,:update]
   get 'users/create'
   get 'users/destroy'
-  
+   devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
